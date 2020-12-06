aaee574029c4412bf510132b13f0bd4f5476fa53
Andrey1408 <55950615+Andrey1408@users.noreply.github.com>
2020-12-06 13:19:41 +0700

    Add files via upload

--- 27ю.py
+++ 27ю.py
@@ -9,8 +9,10 @@ for line in b:
 length = a.pop(0)
 for i in range(0,length):
     for j in range(i+1,length):
-        if ((a[i]-a[j])>0 or (a[j]-a[i])>0 and (a[i]-a[j])%2==0 or (a[j]-a[i])%2==0) and (a[i]%17==0 or a[j]%17==0):
-            if a[i] + a[j] > maxi + maxj:
-                maxi = a[i]
-                maxj = a[j]
+        if (a[i]-a[j])>0 or (a[j]-a[i])>0:
+            if (a[i]-a[j])%2==0 or (a[j]-a[i])%2==0:
+                if a[i]%17==0 or a[j]%17==0:
+                    if a[i] + a[j] > maxi + maxj:
+                        maxi = a[i]
+                        maxj = a[j]
 print(maxi,'',maxj)
